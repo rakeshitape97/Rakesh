98acf261246512d6274897f8b9b2e327a9a7ac2e
Rakesh Itape <rakeshitape844@gmail.com>
2022-03-15 12:08:58 +0530

    new branch code ready to commit and push

--- Tutorial2/src/TestTut2.java
+++ Tutorial2/src/TestTut2.java
@@ -5,10 +5,17 @@ public class TestTut2
 	{
 		System.out.println("This is the new Practice Program...s");
 		
-		
+		System.out.println("Let's go to m1.");
+		TestTut2 t = new TestTut2();
+		t.m1();
 		
 		System.out.println("Code is working fine ready for Push.");
 		
+	}
+	
+	public void m1()
+	{
+		System.out.println("This is the m1 method.");
 	}
 
 }
