d4c405df3b42f63a7ec0a62c4985caccd069d6d7
Rakesh Itape <rakeshitape844@gmail.com>
2022-03-15 12:14:44 +0530

    remove two lines

--- Tutorial2/src/TestTut2.java
+++ Tutorial2/src/TestTut2.java
@@ -2,20 +2,14 @@
 public class TestTut2 
 {
 	public static void main(String[] args) 
-	{
-		System.out.println("This is the new Practice Program...s");
-		
+	{	
 		System.out.println("Let's go to m1.");
 		TestTut2 t = new TestTut2();
-		t.m1();
-		
-		System.out.println("Code is working fine ready for Push.");
-		
+		t.m1();		
 	}
 	
 	public void m1()
 	{
 		System.out.println("This is the m1 method.");
 	}
-
 }
